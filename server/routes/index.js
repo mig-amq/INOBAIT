d5b1e760c26839caf2aea6753257e771f7282d6f
Densolator <raffaeld12@gmail.com>
2019-11-10 07:51:49 +0800

    update of paths and their functionalities
    tangina bakit ba ako pumayag sa pagdev ng javascript putangina hahahahafuck

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+// mongoose.conect();
+var db = mongoose.connection();
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+// db.users.insert({
+//   username: "Patrick Narvasa",
+//   number: 099694201234,
+//   plan: 30,
+//   donated_so_far: 75, //becomes 81 after application of plan
+//   charity: "Philam Foundation"
+// })
+
+// db.updates.insert({
+//   charity: "Philam Foundation",
+//   message: "Thank you for helping Philam Foundation send over 20 students to school! We could not have done this without your help! We look forward to continuing our partnership and continuing to help those who are in need."
+// })
 
 module.exports = router;
